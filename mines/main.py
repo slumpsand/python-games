f910f9479a24f4efc42bf599b98a6d7b9dad1ee7
slumpsand <slumpsand@gmail.com>
2017-07-08 17:20:12 +0200

    do the win / lose thing

--- mines/main.py
+++ mines/main.py
@@ -4,13 +4,16 @@ import pygame, sys, itertools
 from pygame.locals import *
 from pygame import K_ESCAPE, K_F5
 from math import floor
+from random import randint
 
-tiles_x = tiles_y = 20
+tiles_x = tiles_y = 10
 background_color = (180, 180, 180)
-text_color = { "bomb": (255, 0, 0), "common": (255, 255, 255) }
+colors = { "text_bomb": (255, 0, 0), "win_overlay": (0, 255, 0, 160), "lose_overlay": (255, 0, 0, 160) }
 tile_size = 16
 header_size = 40
 bomb_count = 20
+
+[RUNNING, WIN, LOSE] = range(3)
 
 def load_sprites(path, xtiles, ytiles, names, count):
     sprite_img = pygame.transform.scale(pygame.image.load(path), (tile_size * xtiles, tile_size * ytiles))
@@ -31,12 +34,23 @@ def get_field(pos):
         return None
     return x, y
 
+def place_bombs():
+    global bombs
+    bombs = []
+
+    while len(bombs) < bomb_count:
+        b = randint(0, tiles_x-1), randint(0, tiles_y-1)
+        if not b in bombs: bombs.append(b)
+
 def init():
-    global mouse_on, grid, flags
+    global mouse_on, grid, flags, game_state, clicked_bomb
 
     mouse_on = None
+    game_state = RUNNING
+    clicked_bomb = False
     flags = []
     grid = [[0 for _ in range(tiles_y)] for _ in range(tiles_x)]
+    place_bombs()
 
     render()
 
@@ -48,8 +62,8 @@ def quit():
 def render():
     # header
     screen.fill(background_color, Rect(0, 0, screen_size[0], header_size))
-    text = digit_font.render("%d / %d" % (len(flags), bomb_count), 4, text_color["bomb"])
-    (text_x, text_y) = text.get_size()
+    text = digit_font.render("%d / %d" % (len(flags), bomb_count), 4, colors["text_bomb"])
+    text_x, text_y = text.get_size()
     screen.blit(text, ((screen_size[0] - text_x) / 2, (header_size - text_y) / 2))
 
     # all flags / normal / number fields
@@ -65,12 +79,23 @@ def render():
     if mouse_on != None and grid[mouse_on[0]][mouse_on[1]] == 0:
         screen.fill(background_color, tile(mouse_on[0], mouse_on[1]))
 
+    if game_state in [WIN, LOSE]:
+        c = colors["win_overlay"] if game_state == WIN else colors["lose_overlay"]
+        overlay = pygame.Surface((screen_size[0], screen_size[1] - header_size))
+        overlay.fill((c[0], c[1], c[2]))
+        overlay.set_alpha(c[3])
+        screen.blit(overlay, (0, header_size))
+
 def click(pos, is_left):
+    global game_state, clicked_bomb
+
     if pos == None:
         return
 
     if is_left:
-        pass
+        if pos in bombs:
+            game_state = LOSE
+            clicked_bomb = True
     else:
         if grid[pos[0]][pos[1]] in [0, -1]:
             if grid[pos[0]][pos[1]] == -1:
@@ -81,7 +106,13 @@ def click(pos, is_left):
                 grid[pos[0]][pos[1]] = -1
 
 def update():
-    global mouse_on
+    global mouse_on, game_state, clicked_bomb
+
+    should_render = False
+
+    if sorted(bombs) == sorted(flags) and game_state == RUNNING:
+        game_state = WIN
+        should_render = True
 
     for key in [e.key for e in events if e.type == KEYDOWN]:
         if key == K_ESCAPE: quit()
@@ -89,7 +120,12 @@ def update():
             init()
             return True
 
-    should_render = False
+    if clicked_bomb:
+        should_render = True
+        clicked_bomb = False
+
+    if game_state != RUNNING:
+        return should_render
 
     for evt in events:
         curr_mouse_pos = get_field(pygame.mouse.get_pos())
